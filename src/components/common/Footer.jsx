ce6f6936d4364f8d292993277b26f381c2c94a9a
Prado Nicolas <nicolas88prado@gmail.com>
2025-05-13 19:37:22 -0300

    agregue la configuracion de mi db.json

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='py-1 bg-black text-center'> 
-      <p className='text-white fw-bold'>&copy Todos los derechos reservados Independencia Para Tucumán.</p>
+      <p className='text-white fw-bold'>&copy; Todos los derechos reservados Independencia Para Tucumán.</p>
     </div>
   )
 }
