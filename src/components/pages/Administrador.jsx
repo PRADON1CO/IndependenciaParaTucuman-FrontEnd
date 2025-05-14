5887fdfb035587d552cf5d8c7494bd95859613cd
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-05-14 10:19:50 -0300

    agregue el maquetado inicail para el componente Administrador y cree las carpeta personas con sis componente correspondientes

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Administrador = () => {
   return (
     <div className='mainSection'>
-      
+      <div>
+        <div className="">
+          <section className="container mainSection ">
+            <div className="d-flex justify-content-between align-items-center mt-5">
+              <h1 className="text-black display-5 fw-bold ">Gestionar Productos</h1>
+              <Link className="btn bgVerde" to={"/administrador/formulario"}>
+                <i className="bi bi-file-earmark-plus fs-4"></i>
+              </Link>
+            </div>
+            <hr />
+            <Table responsive bordered className="tabla table-striped">
+              <thead>
+                <tr className="text-center">
+                  <th>Cod</th>
+                  <th>Nombre y apellido</th>
+                  <th>Matricula</th>
+                  <th>Domicilio</th>
+                  <th>Imagen</th>
+                  <th>Opciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* <ItemRemera></ItemRemera>
+                <ItemRemera></ItemRemera>
+                <ItemRemera></ItemRemera>
+                <ItemRemera></ItemRemera>
+                <ItemRemera></ItemRemera> */}
+
+              </tbody>
+            </Table>
+          </section>
+        </div>
+      </div>
     </div>
   )
 }
